2b666253bc98b6102e88c7bee46f0aae5001e43c
Patrick Godbey <mrpatg@gmail.com>
2018-01-18 14:35:31 -0500

    update for status codes

--- school.php
+++ school.php
@@ -1,25 +1,45 @@
 <?php
-
-// $remotexml = file_get_contents("https://wvde.state.wv.us/closings/rss/raleigh");
-// $xml = simplexml_load_string($remotexml);
-// if ($xml === false) {
-//     echo "Failed loading XML: ";
-//     foreach(libxml_get_errors() as $error) {
-//         echo "<br>", $error->message;
-//     }
-// } else {
-//     print_r($xml);
-// }
-
-// $xml->channel->
-$remotestring = file_get_contents("https://wvde.state.wv.us/closings/xml/raleigh");
-if (strpos($remotestring, 'closed') !== false) {
-    echo 'closed';
-}else if (strpos($remotestring, 'delay') !== false) {
-    echo 'delay';
-}else{
-    echo "open";
+$remotexml = file_get_contents("https://wvde.state.wv.us/closings/xml/raleigh");
+$xml = simplexml_load_string($remotexml);
+if ($xml === false) {
+    echo "Failed loading XML: ";
+    foreach(libxml_get_errors() as $error) {
+        echo "<br>", $error->message;
+    }
+} else {
+    //print_r($xml);
 }
 
 
-?>
+
+
+foreach($xml->announcement AS $announcement){
+    $date_effective = $announcement->date_effective;
+    //echo $date_effective;
+    $date = new DateTime($date_effective);
+    $now = new DateTime();
+
+    if($date < $now) {
+        if($announcement->status == "1"){
+            echo "delay";
+        }elseif($announcement->status == "2"){
+            echo "delay";
+        }elseif($announcement->status == "3"){
+            echo "early";
+        }elseif($announcement->status == "4"){
+            echo "closed";
+        }elseif($announcement->status == "5"){
+            echo "early";
+        }elseif($announcement->status == "6"){
+            echo "early";
+        }elseif($announcement->status == "7"){
+            echo "early";
+        }elseif($announcement->status == "8"){
+            echo "early";
+        }elseif($announcement->status == "9"){
+            echo "delay";
+        }else{
+            echo "open";
+        }
+    }
+}
